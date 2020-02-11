70dd45eb01267b0a02029fba81b1b78476f53154
Josep M Sobrepere <josepot@gmail.com>
2020-02-11 17:17:23 +0100

    Improve continuousGroupBy implementation

--- src/continuousGroupBy.ts
+++ src/continuousGroupBy.ts
@@ -1,26 +1,26 @@
-import { Observable, Subject, GroupedObservable } from 'rxjs';
+import { Observable, Subject, GroupedObservable, BehaviorSubject } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 const continuousGroupBy = <I, O>(mapper: (x: I) => O) => (
   stream: Observable<I>
 ) =>
   new Observable<GroupedObservable<O, I>>(subscriber => {
     const groups: Map<O, Subject<I>> = new Map();
+
     return stream.subscribe(x => {
       const key = mapper(x);
-      if (!groups.has(key)) {
-        const innerSubject = new Subject<I>();
-        const innerStream = new Observable<I>(innerSubscriber => {
-          innerSubscriber.next(x);
-          return innerSubject
-            .subscribe(innerSubscriber)
-            .add(() => groups.delete(key));
-        }) as GroupedObservable<O, I>;
-        groups.set(key, innerSubject);
-        innerStream.key = key;
-        subscriber.next(innerStream);
-      } else {
-        groups.get(key)!.next(x);
-      }
+
+      if (groups.has(key)) return groups.get(key)!.next(x);
+
+      const subject = new BehaviorSubject<I>(x);
+      groups.set(key, subject);
+
+      const res = subject.pipe(
+        finalize(() => groups.delete(key))
+      ) as GroupedObservable<O, I>;
+      res.key = key;
+
+      subscriber.next(res);
     });
   });
 
