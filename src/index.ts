2faed0995bec36215a621e144b7301a9c67a30ce
Josep M Sobrepere <josepot@gmail.com>
2020-06-01 10:04:54 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ export { default as concatMapUntilData } from './concatMapUntilData';
 export { default as continuousGroupBy } from './continuousGroupBy';
 export { default as scanWithStartingValue } from './scanWithStartingValue';
 export { default as filterNonFalsy } from './filterNonFalsy';
-export { default as plug } from './plug';
 export { default as mergeWhileActive } from './mergeWhileActive';
 export { default as mapDistinct } from './mapDistinct';
 export { default as recursiveObservable } from './recursive-observable';
